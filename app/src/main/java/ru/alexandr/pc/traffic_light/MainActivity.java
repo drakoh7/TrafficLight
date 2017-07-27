2d21e724b09778e1cbe1272caacd730c0670db13
drakoh7 <drakoh7@mail.ru>
2017-07-27 17:40:44 +0500

    Обработчик для 1 кнопки

--- app/src/main/java/ru/alexandr/pc/traffic_light/MainActivity.java
+++ app/src/main/java/ru/alexandr/pc/traffic_light/MainActivity.java
@@ -1,13 +1,27 @@
 package ru.alexandr.pc.traffic_light;
 
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout mConstraintLayout;
+    private TextView mInfoTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mConstraintLayout = (ConstraintLayout) findViewById(R.id.constrainLayout);
+        mInfoTextView = (TextView) findViewById(R.id.textView);
+    }
+
+    public void onRedButtonClick(View view) {
+        mInfoTextView.setText(R.string.green);
+        mConstraintLayout.setBackgroundColor(ContextCompat.getColor(this,R.color.greenColor));
     }
 }
