ff0d1f4b5a42f93d920a273da4725f9ad6bbd125
drakoh7 <drakoh7@mail.ru>
2017-07-28 16:52:14 +0500

    Конечный результат

--- app/src/main/java/ru/alexandr/pc/traffic_light/MainActivity.java
+++ app/src/main/java/ru/alexandr/pc/traffic_light/MainActivity.java
@@ -1,10 +1,12 @@
 package ru.alexandr.pc.traffic_light;
 
+import android.graphics.drawable.Drawable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,10 +20,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mConstraintLayout = (ConstraintLayout) findViewById(R.id.constrainLayout);
         mInfoTextView = (TextView) findViewById(R.id.textView);
+
+        Button yellowButton = (Button) findViewById(R.id.buttonYellow);
+        yellowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mInfoTextView.setText(R.string.yellow);
+                mConstraintLayout.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.yellowColor));
+            }
+        });
     }
 
     public void onRedButtonClick(View view) {
-        mInfoTextView.setText(R.string.green);
-        mConstraintLayout.setBackgroundColor(ContextCompat.getColor(this,R.color.greenColor));
+        mInfoTextView.setText(R.string.red);
+        mConstraintLayout.setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.redColor));
     }
+
+    public void onGreenButtonClick(View view) {
+        mInfoTextView.setText(R.string.green);
+        mConstraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.greenColor));
+    }
+
+
 }
